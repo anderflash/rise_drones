4ec1aeab0b528f59765d3a3d177d481844b89500
Andreas Gising <a.gising@gmail.com>
2023-09-22 12:43:33 +0200

    wip

--- src/dss/auxiliaries/zmq.py
+++ src/dss/auxiliaries/zmq.py
@@ -156,13 +156,6 @@ def demogrify(msg: str) -> typing.Tuple[str, dict]:
 
   return topic, message
 
-def image_file_to_bytes(filename: str) -> bytes:
-  '''t.ex. filename="test.jpg"'''
-  with open(filename, "rb") as fh:
-    data = fh.read()
-    data = base64.b64encode(data)
-  return data
-
 def image_to_bytes(img) -> bytes:
   data = base64.b64encode(img)
   return data
@@ -176,35 +169,6 @@ def string_to_bytes(data: str) -> bytes:
 def bytes_to_image(filename: str, data: bytes) -> None:
   with open(filename, "wb") as fh:
     fh.write(base64.decodebytes(data))
-
-def bytes_to_array_image(filename: str, data: bytes) -> None:
-  print("pre base64 decode")
-  img_array = base64.decodebytes(data)
-  print("pre save")
-  print(f'the filename is {filename}')
-  Image.fromarray(img_array).save('hardcoded.jpg')
-  print("post save")
-  # with open(filename, "wb") as fh:
-  #   pil_img.save(fh)
-
-def numpy_to_bytes(arr: np.array) -> str:
-    arr_dtype = bytearray(str(arr.dtype), 'utf-8')
-    arr_shape = bytearray(','.join([str(a) for a in arr.shape]), 'utf-8')
-    sep = bytearray('|', 'utf-8')
-    arr_bytes = arr.ravel().tobytes()
-    to_return = arr_dtype + sep + arr_shape + sep + arr_bytes
-    return to_return
-
-def bytes_to_numpy(serialized_arr: str) -> np.array:
-    sep = '|'.encode('utf-8')
-    i_0 = serialized_arr.find(sep)
-    i_1 = serialized_arr.find(sep, i_0 + 1)
-    arr_dtype = serialized_arr[:i_0].decode('utf-8')
-    arr_shape = tuple([int(a) for a in serialized_arr[i_0 + 1:i_1].decode('utf-8').split(',')])
-    arr_str = serialized_arr[i_1 + 1:]
-    arr = np.frombuffer(arr_str, dtype = arr_dtype).reshape(arr_shape)
-    return arr
-
 
 def save_json(filename: str, data: dict) -> None:
   with open(filename, "w") as fh:
